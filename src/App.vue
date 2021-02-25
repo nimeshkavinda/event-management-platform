3a6c7ef45ada711752739649d0229ac0b8b70982
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-02-25 20:11:53 +0530

    add nav bar and (have to complete) loginform (not complete)

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,52 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="blue"
       dark
     >
+    <v-title>Logo</v-title>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="FOSS Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src=""
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        
+       
       </div>
+      
+      <v-btn text>Home</v-btn>
+      <v-btn text>About</v-btn>
 
-      <v-spacer></v-spacer>
+       <v-spacer></v-spacer>
+      <v-btn text>Login</v-btn>
+      <v-btn text>SinUp</v-btn>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+
+      
+     
+
+      
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+     <LoginForm/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
+import LoginForm from './components/LoginForm';
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    LoginForm
   },
 
   data: () => ({
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,45 @@
 <template>
+
+      
+<form>
     
+     
+        <v-container>
+              <v-spacer></v-spacer>
+          
+                <v-col cols="12" sm="6">
+                    <v-text-field>
+                        v-model="mail"
+                        :error-messages="mailErrors"
+                        label="Email address"
+                        
+                        solo
+                        required
+                    </v-text-field>
+
+                </v-col>
+
+                <v-col cols="12" sm="6">
+                    <v-text-field >
+                        v-model="Password"
+                        lable="Password"
+                        solo
+                        required
+                    </v-text-field>
+                    
+                    <v-text>Frogget Password</v-text>
+
+                    <v-btn class="mr-4" @click="Sigin">
+                        Sigin
+
+                    </v-btn>
+
+                </v-col>
+         
+        </v-container>
+    
+  </form>
+  
 </template>
 
 <script>
