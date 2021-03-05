aca9e7224ff51e9e3ed0ece4f89d5e3d16c3b07f
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-03-05 14:31:54 +0530

    login and signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Sign from '../views/Sign.vue'
+
 
 Vue.use(VueRouter)
 
@@ -17,7 +20,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+  {
+    path: 'sign',
+    name: 'Sign',
+    component: Sign
+  },
+ 
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+<mdb-container>
+    <mdb-row>
+        <mdb-col col="6">
+               <p class="text-center ">Sign in to<br>Our Latest<br>Event Management Platform</p>
+                  <p class="text-center ">Register here if you don't<br>have an account yet</p>
+        
+
+        </mdb-col>
+
+        <mdb-col col="6">
+              <!-- Default form login -->
+  <form>
+    
+    <label for="defaultFormLoginEmailEx" class="grey-text">Your email</label>
+    <input type="email" id="defaultFormLoginEmailEx" class="form-control"/>
+    <br/>
+    <label for="defaultFormLoginPasswordEx" class="grey-text">Your password</label>
+    <input type="password" id="defaultFormLoginPasswordEx" class="form-control"/>
+       <p class="text-center mb-4">Foget my password</p>
+    <div class="text-center mt-4">
+      <button class="btn btn-indigo" type="submit">Login</button>
+    </div>
+ 
+  </form>
+  <!-- Default form login -->
+
+
+        </mdb-col>
+    </mdb-row>
+
+</mdb-container>
+
+    
+</template>
+
+<script>
+import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
+export default {
+  name: "AboutPage",
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+  },
+};
+</script>
