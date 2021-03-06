15223e5da44f5d8e460fa37adc6630927f880cca
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-06 14:44:33 +0530

    ghost api

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <mdb-row class="mb-4">
+    <mdb-stretched-link :to="post.url">
+      <mdb-card style="box-shadow: none!important;">
+        <mdb-row>
+          <mdb-col col="6">
+            <img
+              :src="post.feature_image"
+              alt="thumbnail"
+              width="100%"
+              height="180px"
+              style="border-radius: 0.6em; object-fit: cover;"
+            />
+          </mdb-col>
+          <mdb-col col="6">
+            <div class="card-body">
+              <h5 class="h5 font-weight-bold">
+                {{ post.title }}
+              </h5>
+              <mdb-row>
+                <mdb-col col="1">
+                  <img
+                    :src="post.primary_author.profile_image"
+                    class="rounded-circle"
+                    height="22"
+                    width="22"
+                    alt=""
+                    loading="lazy"
+                  />
+                </mdb-col>
+                <mdb-col col="10">
+                  <p class="card-text">
+                    {{ post.primary_author.name }}
+                  </p>
+                </mdb-col>
+              </mdb-row>
+            </div>
+          </mdb-col>
+        </mdb-row>
+      </mdb-card>
+    </mdb-stretched-link>
+  </mdb-row>
+</template>
+
+<script>
+import { mdbRow, mdbCol, mdbCard, mdbStretchedLink } from "mdbvue";
+export default {
+  name: "ArticleCard",
+  props: ["post"],
+  components: {
+    mdbRow,
+    mdbCol,
+    mdbCard,
+    mdbStretchedLink
+  },
+};
+</script>
