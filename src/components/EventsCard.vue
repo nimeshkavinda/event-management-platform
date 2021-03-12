b11c285ef14fea4636fa60b0f3010f29d6fef388
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-03-12 14:24:16 +0530

    eventscard

--- src/components/EventsCard.vue
+++ src/components/EventsCard.vue
@@ -1,29 +1,33 @@
 <template>
-  <mdb-card style="border-radius: 0.5em;">
-    <mdb-card-body class="p-4">
-      <mdb-card-title class="h1 font-weight-bold">{{
-        event.title
-      }}</mdb-card-title>
-      <mdb-card-text class="h1 mb-5"> {{ event.desc }}</mdb-card-text>
-      <mdb-card-text class="h1 blue-text">{{ event.venue }}</mdb-card-text>
-      <mdb-card-title class="h4 font-weight-bold"
-        >{{ event.date }} {{ event.time }}</mdb-card-title
-      >
-      <mdb-btn :to="event.url" color="light-green" style="border-radius:1em;">RSVP</mdb-btn>
-    </mdb-card-body>
-  </mdb-card>
+  <div>
+    <router-link :to="'/events/' + eventscard.id" class="black-text">
+      <mdb-card style="border-radius: 0.6em;">
+        <mdb-card-body>
+          <mdb-card-title class="h5 font-weight-bold">
+            {{ eventscard.title }}
+          </mdb-card-title>
+          <mdb-card-text class="h1 mb-3"> {{ eventscard.desc }}</mdb-card-text>
+          <mdb-card-text class="h1 blue-text">{{
+            eventscard.venue
+          }}</mdb-card-text>
+          <mdb-card-title class="h4 font-weight-bold"
+            >{{ event.date }} {{ event.time }}</mdb-card-title>
+            <mdb-btn :to="event.url" color="light-green" style="border-radius:1em;">RSVP</mdb-btn>
+        </mdb-card-body>
+      </mdb-card>
+    </router-link>
+  </div>
 </template>
 
 <script>
-import { mdbCard, mdbCardTitle, mdbCardText, mdbCardBody } from "mdbvue";
+import { mdbCard, mdbCardBody, mdbCardTitle } from "mdbvue";
 export default {
   name: "EventsCard",
-  props: ["EventsCard"],
+  props: ["eventscard"],
   components: {
     mdbCard,
-    mdbCardTitle,
-    mdbCardText,
     mdbCardBody,
+    mdbCardTitle,
   },
 };
 </script>
--- src/views/Events.vue
+++ src/views/Events.vue
@@ -8,54 +8,74 @@
       <mdb-col md="7" class="mt-2 pl-lg-4">
         <h6>
           Our community events are for students and enthusiasts to learn
-          discaver new technoalogies 
-          Having doubts?Please contact <mdb-stretched-link href="#!" class="stretched-link">info@fossnsbm.org </mdb-stretched-link>
-        </h6>  
+          discaver new technoalogies Having doubts?Please contact
+          <mdb-stretched-link href="#!" class="stretched-link"
+            >info@fossnsbm.org
+          </mdb-stretched-link>
+        </h6>
       </mdb-col>
     </mdb-row>
-    
+
     <mdb-row class="mx-5 my-5 font-weight-bold">
-      <mdb-col class="md-5" sm="4">
+      <mdb-col sm="4">
         <p class="h5 blue-text font-weight-bold mt-2">
           Upcomming Events
         </p>
-       <EventsCard></EventsCard>
-
+        <EventsCard
+          v-for="events in events.slice(3)"
+          :key="events.id"
+          :eventscard="events"
+        ></EventsCard>
       </mdb-col>
-      
-
     </mdb-row>
 
     <mdb-row class="mx-5 my-5">
       <mdb-col md="5">
         <p class="h5 blue-text font-weight-bold mt-2">
-          Frequently Asked <br>Questions
+          Frequently Asked <br />Questions
         </p>
-        
-      </mdb-col>
-      
-      <mdb-col md="7" class="mt-3">
-        
-        <hr>
-        <h5 class="font-weight-bold">Who are the events for?<mdb-icon class="align-text-top" icon="angle-down" /></h5>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       <hr>
-       
-       <h5 class="font-weight-bold">I'm from a different university. Can I still participate?<mdb-icon icon="plus" /></h5>
-       
-       <hr>
-       <h5 class="font-weight-bold">How do I RSVP for events?     <mdb-icon icon="plus" /></h5>
-       
       </mdb-col>
 
-      
+      <mdb-col md="7" class="mt-3">
+        <hr />
+        <h5 class="font-weight-bold">Who are the events for?</h5>
+        <mdb-accordion :panes="panes" material></mdb-accordion>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <hr />
+
+        <h5 class="font-weight-bold">
+          I'm from a different university. Can I still participate?<mdb-icon
+            icon="plus"
+          />
+        </h5>
+
+        <hr />
+        <h5 class="font-weight-bold">
+          How do I RSVP for events? <mdb-icon icon="plus" />
+        </h5>
+      </mdb-col>
     </mdb-row>
   </mdb-container>
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol, mdbIcon,mdbStretchedLink} from "mdbvue";
+import {
+  mdbContainer,
+  mdbRow,
+  mdbCol,
+  mdbIcon,
+  mdbStretchedLink,
+  mdbAccordion,
+} from "mdbvue";
 import EventsCard from "../components/EventsCard.vue";
 export default {
   name: "Events",
@@ -64,8 +84,14 @@ export default {
     mdbRow,
     mdbCol,
     mdbIcon,
-    mdbStretchedLink, 
-    EventsCard
+    mdbStretchedLink,
+    EventsCard,
+    mdbAccordion,
+  },
+  data() {
+    return {
+      panes: [],
+    };
   },
 };
 </script>
