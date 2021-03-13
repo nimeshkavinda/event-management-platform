94d36bcb7c37a9a5bf3038fb957b860362547cd2
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-13 12:04:31 +0530

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,22 +2,22 @@
   <div>
     <mdb-container fluid>
       <mdb-row class="mx-5 mt-5">
-        <mdb-col lg="7" class="py-5 mt-5"
+        <mdb-col lg="6" class="py-5 mt-5 ml-lg-5"
           ><p
             class="h1 light-green-text text-left font-weight-bold"
-            style="font-size:8vw;"
+            style="font-size:10vw;"
           >
             Oops!
           </p>
-          <p class="h3 text-left font-weight-bold">
+          <p class="h2 text-left font-weight-bold">
             We can't seem to find the page you are looking for.
           </p>
-          <p class="h6 text-left font-weight-bold blue-text">
+          <p class="h5 text-left font-weight-bold blue-text">
             <a @click="$router.go(-1)">Go Back</a>
           </p></mdb-col
         ><mdb-col lg="5"
           ><div
-            class="tenor-gif-embed"
+            class="tenor-gif-embed ml-lg-5"
             data-postid="14949853"
             data-share-method="host"
             data-width="100%"
