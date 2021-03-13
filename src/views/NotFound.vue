b5b9d7e2422da33d1980a53f6a4ff80083bdf32e
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-13 11:58:54 +0530

    error page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,51 @@
 <template>
-    <div>404</div>
+  <div>
+    <mdb-container fluid>
+      <mdb-row class="mx-5 mt-5">
+        <mdb-col lg="7" class="py-5 mt-5"
+          ><p
+            class="h1 light-green-text text-left font-weight-bold"
+            style="font-size:8vw;"
+          >
+            Oops!
+          </p>
+          <p class="h3 text-left font-weight-bold">
+            We can't seem to find the page you are looking for.
+          </p>
+          <p class="h6 text-left font-weight-bold blue-text">
+            <a @click="$router.go(-1)">Go Back</a>
+          </p></mdb-col
+        ><mdb-col lg="5"
+          ><div
+            class="tenor-gif-embed"
+            data-postid="14949853"
+            data-share-method="host"
+            data-width="100%"
+            data-aspect-ratio="1.0"
+          >
+            <a
+              href="https://tenor.com/view/verdrietig-sad-cry-lonely-gif-14949853"
+              >Verdrietig Sad GIF</a
+            >
+            from
+            <a href="https://tenor.com/search/verdrietig-gifs"
+              >Verdrietig GIFs</a
+            >
+          </div>
+        </mdb-col></mdb-row
+      ></mdb-container
+    >
+  </div>
 </template>
 
 <script>
+import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
 export default {
-    name: "NotFoundPage"
-}
+  name: "NotFoundPage",
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+  },
+};
 </script>
